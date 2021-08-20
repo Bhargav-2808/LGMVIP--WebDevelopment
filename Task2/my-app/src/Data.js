0f63060d1d487c237e6171ba33fef9ef6d7050d7
Bhargav-2808 <bhargavvalani01@gmail.com>
2021-08-20 18:36:47 +0530

    update state

--- Task2/my-app/src/Data.js
+++ Task2/my-app/src/Data.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React from "react";
+import { useState,useEffect } from "react";
 
 
-const Data = () =>{
-    return(
-        <>
-            <h1>Hello</h1>
-        </>
-    );
 
-}
+const Data = () => {
+    const [users, setUsers] = useState([]);
+
+    const url="https://reqres.in/api/users?page=1";
+    
+    useEffect(() => {
+        
+     loadusers();
+    }, [])
+    
+    const loadusers = async ()=>{
+        //console.log("before");
+        const response = await fetch(url);
+        const result= await response.json();
+        console.log(result.data);
+        setUsers(result.data);
+    }
+    
+    
+  
+  return (
+    <>
+        <button onClick={loadusers} > getData</button>
+        <ul>
+            {
+                users.map((data)=>(
+                        <p>{data.email}</p>
+                    ))
+            }
+        </ul> 
+ 
+    </> 
+  );
+};
 
 export default Data;
