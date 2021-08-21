214556fd373771f3441b2566f2dab50055ec53b9
Bhargav-2808 <bhargavvalani01@gmail.com>
2021-08-21 17:01:06 +0530

    adding card

--- Task2/my-app/src/Data.js
+++ Task2/my-app/src/Data.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { useState ,useEffect} from "react";
+import { useState, useEffect } from "react";
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
 
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 //import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-
+import './Data.css'
 const useStyles = makeStyles((theme) => ({
     backdrop: {
         zIndex: theme.zIndex.drawer + 1,
@@ -40,7 +41,7 @@ const Data = () => {
     return (
         <>
             <button onClick={loadusers} > getData</button>
-            <ul>
+            {/* <ul>
                 {
                     users.map((data, key) => (
                         <p key={key}>
@@ -52,8 +53,26 @@ const Data = () => {
                         </p>
                     ))
                 }
-            </ul>
+            </ul> */}
+            {
+                users.map((data, key) => {
+                    return (
+                        <div>
 
+                        <Card style={{ width: '12rem' }}>
+                            <Card.Img variant="top" src={data.avatar} />
+                            <Card.Body>
+                                <Card.Title>{data.first_name} {data.last_name}</Card.Title>
+                                <Card.Text>
+                                    {data.email}
+                                </Card.Text>
+                            </Card.Body>
+
+                        </Card>
+                        </div>
+                    )
+                })
+            }
 
         </>
     );
